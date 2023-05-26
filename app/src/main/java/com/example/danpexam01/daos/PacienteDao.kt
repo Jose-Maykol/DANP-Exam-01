96ac507762d3aeaf19ce039e9ba746c6c1c3aae9
rhuarancca <rhuarancca@unsa.edu.pe>
2023-05-25 21:32:42 -0500

    crear paciente

--- app/src/main/java/com/example/danpexam01/daos/PacienteDao.kt
+++ app/src/main/java/com/example/danpexam01/daos/PacienteDao.kt
@@ -5,12 +5,17 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.danpexam01.models.Paciente
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PacienteDao {
     @Insert
     suspend fun insertPaciente(paciente: Paciente)
+
+    @Query("UPDATE pacientes set nombre=:nombre WHERE idPaciente=:idPaciente")
+    suspend fun actualizarPaciente(idPaciente: Int, nombre: String)
+
+    @Query("DELETE FROM pacientes WHERE idPaciente=:idPaciente")
+    suspend fun borrarPaciente(idPaciente: Int)
 
     @Query("SELECT * FROM pacientes")
     fun getAllPacientes(): LiveData<List<Paciente>>
