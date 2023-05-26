6fc17ec0508f8120b312825f42892880aeab46da
paulina <pchoqueneira@unsa.edu.pe>
2023-05-26 00:07:54 -0500

    form y body juntos - citas

--- app/src/main/java/com/example/danpexam01/components/HacerCita.kt
+++ app/src/main/java/com/example/danpexam01/components/HacerCita.kt
@@ -1,23 +1,16 @@
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
-import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
 import androidx.navigation.NavHostController
@@ -25,54 +18,17 @@ import com.example.danpexam01.models.Paciente
 import java.util.*
 
 @Composable
-fun HacerCita(
-    navController: NavHostController) {
-    var fecha by remember { mutableStateOf("") }
+fun HacerCita(navController: NavHostController) {
+    /*var fecha by remember { mutableStateOf("") }
     var hora by remember { mutableStateOf("") }
     var isEditando by remember { mutableStateOf(false) }
     var textButton by remember { mutableStateOf("Guardar") }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Crear una cita",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .wrapContentSize(Alignment.Center)
-                    ) },
-                backgroundColor = Color.Transparent,
-                contentColor = Color.White,
-                modifier = Modifier.padding(top = 15.dp),
-                elevation = 0.dp,
-                actions = {
-                    IconButton(
-                        onClick = {
-                            navController?.navigate("hacerCita_screen")}
-                    ) {
-                        Icon(
-                            imageVector = Icons.Default.Add,
-                            contentDescription = "Add"
-                        )
-                    }
-                }
-            )
-        }
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(12.dp)
-        ) {
-            formCita()
-        }
-    }
+    ///formCita(navController?.navigate("hacerCita_screen")
 }
 @Composable
-fun formCita(){
-    var paciente: Paciente
+public fun formCita(navController: Unit){*/
+    val paciente = remember { mutableStateListOf<Paciente>() }
     var idPaciente by remember { mutableStateOf("") }
     var nombre by remember { mutableStateOf("") }
     var apellido by remember { mutableStateOf("") }
@@ -84,24 +40,32 @@ fun formCita(){
     var doctor by remember { mutableStateOf("") }
     var fechaCita by remember { mutableStateOf("") }
     var horaCita by remember { mutableStateOf("") }
+    var guardar by remember { mutableStateOf("Guardar") }
 
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(14.dp)
-    ) { /*nombre = paciente.nombre
-        apellido = paciente.apellido
-        genero = paciente.genero
-        fechaNacimiento = paciente.fechaNacimiento
-        telefono = paciente.telefono
-        direccion = paciente.direccion
-        email = paciente.email*/
-
+        modifier = Modifier.padding(32.dp).fillMaxHeight(),
+        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         ElegirDoctor(doctor)
         DatePicker(fechaCita)
-        Spacer(modifier = Modifier.padding(vertical = 4.dp))
         DateTimePicker(horaCita)
+        /*nombre = paciente.nombre,
+        apellido = paciente.apellido,
+        genero = paciente.genero,
+        fechaNacimiento = paciente.fechaNacimiento,
+        telefono = paciente.telefono,
+        direccion = paciente.direccion,
+        email = paciente.email,*/
+        Button(
+            onClick = {
+                navController?.navigate("home_screen")
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Registrar")
+        }
+        ///Spacer(modifier = Modifier.padding(vertical = 4.dp))
     }
 }
 @Composable
@@ -183,27 +147,21 @@ fun ElegirDoctor(doctor: String) {
     var expanded by remember { mutableStateOf(false) }
     val doctoresCombo = arrayOf("Dr. Daniel Ventura", "Dra. Rafaela Vera", "Dra. Carmen Expresso", "Dr. Manuel Carrasco", "Dr. Carlos Conde")
     var doctor by remember { mutableStateOf("") }
-    var textfieldSize by remember { mutableStateOf("")}
 
     val icon = if (expanded)
         Icons.Filled.KeyboardArrowUp
     else
         Icons.Filled.KeyboardArrowDown
 
-    Column(Modifier.padding(1.dp)) {
+    Column(Modifier.padding(0.dp).fillMaxWidth()) {
         OutlinedTextField(
             value = doctor,
             onValueChange = { doctor = it },
             readOnly = true,
             modifier = Modifier
-                .fillMaxWidth()
-                .onGloballyPositioned { coordinates ->
-                    //This value is used to assign to the DropDown the same width
-                    textfieldSize = coordinates.size
-                        .toSize()
-                        .toString()
-                },
-            label = {Text("Label")},
+                .width(IntrinsicSize.Max),
+
+            label = {Text("Especialista")},
             trailingIcon = {
                 Icon(icon,"contentDescription",
                     Modifier.clickable { expanded = !expanded })
